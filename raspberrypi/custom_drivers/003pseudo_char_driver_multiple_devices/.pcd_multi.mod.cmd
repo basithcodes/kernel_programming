a2ca4a923b8f46ca6035887d734c3985b4396f64
Abdul Basith <abdulbasith.eee@hotmail.com>
2021-02-28 10:18:45 +0530

    deleted:    .Module.symvers.cmd 	deleted:    .modules.order.cmd 	deleted:    .pcd_multi.ko.cmd 	deleted:    .pcd_multi.mod.cmd 	deleted:    .pcd_multi.mod.o.cmd 	deleted:    .pcd_multi.o.cmd 	deleted:    Module.symvers 	deleted:    compile_commands.json 	deleted:    modules.order 	deleted:    pcd_multi.ko 	deleted:    pcd_multi.mod 	deleted:    pcd_multi.mod.c 	deleted:    pcd_multi.mod.o 	deleted:    pcd_multi.o

--- raspberrypi/custom_drivers/003pseudo_char_driver_multiple_devices/.pcd_multi.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/basith/Coding/kernel_programming/raspberrypi/custom_drivers/003pseudo_char_driver_multiple_devices/pcd_multi.mod := { echo  /home/basith/Coding/kernel_programming/raspberrypi/custom_drivers/003pseudo_char_driver_multiple_devices/pcd_multi.o;  echo; } > /home/basith/Coding/kernel_programming/raspberrypi/custom_drivers/003pseudo_char_driver_multiple_devices/pcd_multi.mod
